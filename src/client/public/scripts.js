cdd1a1c2eb2bfd01f7133dca37db530aa52b6a74
Luiz Eduardo Campos da Silva <luizcampos331@gmail.com>
2023-11-12 16:54:04 -0300

    feat: send message from client to server

--- src/client/public/scripts.js
+++ src/client/public/scripts.js
@@ -1,11 +1,42 @@
+function waitLoadDOM(callback) {
+  document.addEventListener('DOMContentLoaded', callback());
+}
+
+function sendMessage(socket) {
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('message').value = '';
+
+    socket.send(JSON.stringify({ name, email, phone, message }));
+  });
+}
+
 function websocketHandler() {
   const socket = new WebSocket('_SERVER_URL_');
 
-  // const messages = document.getElementById('messages');
+  socket.onopen = () => {
+    console.info('Connected');
 
-  // socket.onmessage = message => (messages.innerHTML += message.data);
-  socket.onopen = event => console.info('Connected', event);
-  socket.onmessage = message => console.log('message', message);
+    waitLoadDOM(() => {
+      sendMessage(socket);
+    });
+  };
+
+  socket.onmessage = message => {
+    console.log('message', message);
+  };
+
   socket.onerror = error => console.error(`WebSocket error:`, error);
   socket.onclose = () => console.info('Disconnected from WebSocket server');
 }
--- src/server/web-socket-handler.ts
+++ src/server/web-socket-handler.ts
@@ -2,6 +2,11 @@ import { IncomingMessage } from 'http';
 import crypto from 'node:crypto';
 import internal from 'stream';
 
+const SEVEN_BITS_INTEGER_MARKER = 125;
+const SIXTEEN_BITS_INTEGER_MARKER = 126;
+const MASK_KEY_BYTES_LENGTH = 4;
+const FIRST_BIT = 128; // parseInt('10000000', 2)
+
 class WebSocketHandler {
   private createSocketAccept(id: string): string {
     const sha1 = crypto.createHash('sha1');
@@ -26,12 +31,51 @@ class WebSocketHandler {
     return headers;
   }
 
+  private unmask(encodedBuffer: Buffer, maskKey: Buffer) {
+    const finalBuffer = Buffer.from(encodedBuffer);
+
+    for (let index = 0; index < encodedBuffer.length; index++) {
+      finalBuffer[index] =
+        encodedBuffer[index] ^ maskKey[index % MASK_KEY_BYTES_LENGTH];
+    }
+
+    return finalBuffer;
+  }
+
+  private readableSocket(socket: internal.Duplex) {
+    socket.read(1);
+
+    const [markerAndPayloadLength] = socket.read(1);
+    const lengthIndicatorInBits = markerAndPayloadLength - FIRST_BIT;
+
+    let messageLength = 0;
+
+    if (lengthIndicatorInBits <= SEVEN_BITS_INTEGER_MARKER) {
+      messageLength = lengthIndicatorInBits;
+    } else if (lengthIndicatorInBits === SIXTEEN_BITS_INTEGER_MARKER) {
+      messageLength = socket.read(2).readUint16BE(0);
+    } else {
+      throw new Error(
+        `your message is too long! we don't handle 64-bit messages`,
+      );
+    }
+
+    const maskKey = socket.read(MASK_KEY_BYTES_LENGTH);
+    const encoded = socket.read(messageLength);
+    const decoded = this.unmask(encoded, maskKey);
+    const received = decoded.toString('utf8');
+
+    const data = JSON.parse(received);
+    console.log('Message:', data);
+  }
+
   public upgrade = (req: IncomingMessage, socket: internal.Duplex) => {
     const { 'sec-websocket-key': webClientSocketKey } = req.headers;
 
     const headers = this.prepareHeaders.bind(this)(webClientSocketKey);
 
     socket.write(headers);
+    socket.on('readable', () => this.readableSocket(socket));
   };
 }
 
